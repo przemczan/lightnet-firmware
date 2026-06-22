82075397bbd9fe5dd29080336bcb034b4d3da185
Przemyslaw Telazka <przemczan@gmail.com>
2026-06-22 17:29:29 +0200

    s2 config fix

--- src/controller/config.hpp
+++ src/controller/config.hpp
@@ -31,25 +31,6 @@
     #ifndef PANELS_POWER_PIN
         #define PANELS_POWER_PIN 14
     #endif
-#elif defined(ARDUINO_ARCH_ESP32)
-    #ifndef INITIALIZER_EDGE_PIN_NO
-        #define INITIALIZER_EDGE_PIN_NO 12
-    #endif
-    #ifndef INITIALIZER_EDGE_INTERRUPT_PIN_NO
-        #define INITIALIZER_EDGE_INTERRUPT_PIN_NO 13
-    #endif
-    #ifndef LED_PIN
-        #define LED_PIN 2
-    #endif
-    #ifndef IIC_SDA_PIN
-        #define IIC_SDA_PIN 4
-    #endif
-    #ifndef IIC_SCL_PIN
-        #define IIC_SCL_PIN 5
-    #endif
-    #ifndef PANELS_POWER_PIN
-        #define PANELS_POWER_PIN 21
-    #endif
 #elif defined(ARDUINO_LOLIN_S2_MINI)
     #ifndef INITIALIZER_EDGE_PIN_NO
         #define INITIALIZER_EDGE_PIN_NO 11
@@ -68,6 +49,25 @@
     #endif
     #ifndef PANELS_POWER_PIN
         #define PANELS_POWER_PIN 7
+    #endif
+#elif defined(ARDUINO_ARCH_ESP32)
+    #ifndef INITIALIZER_EDGE_PIN_NO
+        #define INITIALIZER_EDGE_PIN_NO 12
+    #endif
+    #ifndef INITIALIZER_EDGE_INTERRUPT_PIN_NO
+        #define INITIALIZER_EDGE_INTERRUPT_PIN_NO 13
+    #endif
+    #ifndef LED_PIN
+        #define LED_PIN 2
+    #endif
+    #ifndef IIC_SDA_PIN
+        #define IIC_SDA_PIN 4
+    #endif
+    #ifndef IIC_SCL_PIN
+        #define IIC_SCL_PIN 5
+    #endif
+    #ifndef PANELS_POWER_PIN
+        #define PANELS_POWER_PIN 21
     #endif
 #else
     #ifndef INITIALIZER_EDGE_PIN_NO
